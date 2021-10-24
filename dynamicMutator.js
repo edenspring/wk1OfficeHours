f627b65451e4c9236edbb5687aa96b25d8f334b4
Bill Adams <vikingbill@gmail.com>
2021-10-24 14:49:18 -0400

    add prompts

---
+++ dynamicMutator.js
@@ -0,0 +1,27 @@
+/*
+Write a function that takes in an array, a string representing the name of a method,
+and three optional parameters. The function should be written to handle these methods:
+
+* pop
+    - Mutate the array by popping off the last element
+    - Return the mutated array
+* shift
+    - Mutate the array by removing the 0th element in the array
+    - Return the mutated array
+* push 
+    - Utilize the first optional parameter to specify something to be pushed into the array
+    - After pushing into said array, return mutated array
+* unshift
+    - Utilize the first optional paramter to specify something to be unshifted into the array
+    - After inserting something in as the new 0th element, return mutated array
+* slice
+    - Utilize the first and second optional parameters to specify a beginning and ending index
+    - return the result of calling splice on the given array with the specified indices
+* splice
+    - Utilize all optional parameters to specify:
+        - start index
+        - how many things to remove
+        - what to add in
+    - Return the result of calling the splice operation with the supplied arguments
+*/
+
---
+++ mostVowels.js
@@ -0,0 +1,9 @@
+/*
+This prompt will have two functions:
+
+* vowelCounter - a function that returns the total number of vowels in a given sentence
+
+* mostVowelsFinder - a function that takes in an array of sentences and returns the
+  index of the sentence that contains the most vowels
+
+*/
