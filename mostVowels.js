802fd092b2f89d3a3d068345695992b12a5574ad
Bill Adams <vikingbill@gmail.com>
2021-10-24 15:09:33 -0400

    add clarification

--- mostVowels.js
+++ mostVowels.js
@@ -7,3 +7,16 @@ This prompt will have two functions:
   index of the sentence that contains the most vowels
 
 */
+
+function vowelCounter(sentence) {
+
+}
+
+function mostVowelsFinder(sentenceArray){
+
+}
+
+let sentence1 = "Bootcamp life is difficult"
+let sentence2 = "JavaScript is a fun language"
+let sentence3 = "Hope you all had a good weekend"
+let sentence4 = "Good luck on Monday's assessment, you got this!"
